df3e175c992f89207c0fbca8d4a05c023833270e
unknown <Alunos@LAB04-PC16.academico.formiga.ifmg.local>
2023-11-06 17:06:29 -0300

    Teste AFEQ

--- Trabalho1/main.py
+++ Trabalho1/main.py
@@ -110,4 +110,21 @@ if __name__ == '__main__':
     af2.criaTransicao(4, 4, 'b')
 
     print(Minimizacao.afEq(af1, af2))
+    
+    print('***************************************************')
+    
+    af5 = AFD('ab')
+    af5.criaEstado(1, True)
+    af5.criaEstado(2, False, True)
+    af5.criaTransicao(1,2,'b')
+    af5.criaTransicao(2,1,'a')
+    
+    af6 = AFD('ab')
+    af6.criaEstado(1, True)
+    af6.criaEstado(2, False, True)
+    af6.criaTransicao(1,2,'b')
+    af6.criaTransicao(2,1,'b')
+    
+    print(Minimizacao.afEq(af5, af6))
+    
 
